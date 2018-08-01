7d3e9773ec6f415a45b7c5f66164b9d23b144316
ilgerodias <igerodias@hotmail.com>
2018-08-01 12:55:39 -0700

    it broke again

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ $(document).ready(function(){
 
   console.log('ready and waiting!')
 
-  $(".test").click(function(event){
-    console.log(event)
-    $(this).fadeOut();
+  $(".test").click(function(){
 
+    $(this).fadeOut();
   })
 
 })
